d1f6f714958fbf62dd5c6508481c7b8c2f65d67d
shifasageer <133022028+shifasageer@users.noreply.github.com>
2024-09-18 10:54:35 +0530

    Add files via upload

---
+++ BootSecurityJWT/src/main/java/com/test/jwt/model/AuthenticationResponse.java
@@ -0,0 +1,27 @@
+package com.test.jwt.model;
+
+public class AuthenticationResponse {
+
+	private final String jwt;
+
+
+
+	public AuthenticationResponse(String jwt) {
+
+		
+
+		this.jwt = jwt;
+
+	}
+
+
+
+	public String getJwt() {
+
+		return jwt;
+
+	}
+
+
+
+}
